28b61f28774255903c0eb2bec05d564dbbb1a899
Amel Dussier <amel.dussier@gmail.com>
2016-06-05 16:28:00 +0200

    Step 5

---
+++ docker-images/auto_step_5.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# This is a bit brutal (and will affect your system if you are running other
+# containers than those of the lab)
+#
+docker kill $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+#
+# Remove the docker images
+#
+docker rmi res/apache_static
+docker rmi res/express_students
+docker rmi res/apache_reverse_proxy
+
+#
+# Build our docker images
+#	
+docker build --tag res/apache_static --file ./apache-php-image/Dockerfile ./apache-php-image/
+docker build --tag res/express_students --file ./express-image/Dockerfile ./express-image/
+docker build --tag res/apache_reverse_proxy --file ./apache-reverse-proxy/Dockerfile ./apache-reverse-proxy/
+
+#
+# Start containers
+#
+docker run -d --name apache_static res/apache_static
+docker run -d --name express_students res/express_students
+docker run -d --name apache_reverse_proxy -e STATIC_APP=172.17.0.2:80 -e DYNAMIC_APP=172.17.0.3:3000 -p 8080:80 res/apache_reverse_proxy
